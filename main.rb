bca8d2b5f0dc1c71e87c261e871af0c42c4e5f20
svobom57 <svobom57@gmail.com>
2015-03-27 21:37:28 +0100

    Fix benchmark

--- main.rb
+++ main.rb
@@ -3,12 +3,12 @@ require_relative 'evolution'
 require_relative 'graph_config'
 require 'benchmark'
 
-graph = Graph.new(get_vertices, get_edges)
-evolution = Evolution.new(graph, 20, 0.90, 0.05, 100)
 iterations = 10
 Benchmark.bm do |bm|
   bm.report do
     iterations.times do
+      graph = Graph.new(get_vertices, get_edges)
+      evolution = Evolution.new(graph, 20, 0.90, 0.05, 100)
       p evolution.evolve_min_vertex_cover
     end
   end
