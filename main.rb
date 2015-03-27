914a1bc79f2a1e1394d6d79fb468ba8f871357ad
svobom57 <svobom57@gmail.com>
2015-03-27 20:47:49 +0100

    Remove unnecessary parentheses

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require 'benchmark'
 graph = Graph.new(get_vertices, get_edges)
 evolution = Evolution.new(graph, 20, 0.90, 0.05, 100)
 iterations = 10
-Benchmark.bm() do |bm|
+Benchmark.bm do |bm|
   bm.report do
     iterations.times do
       p evolution.evolve_min_vertex_cover
